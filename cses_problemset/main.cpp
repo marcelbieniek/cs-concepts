bebdf100046e1a3b51f10f2ab93c85919b82fa0f
Marcel Bieniek <marcel.bieniek@gmail.com>
2025-02-06 15:27:01 +0100

    implement Missing Number task

--- cses_problemset/main.cpp
+++ cses_problemset/main.cpp
@@ -15,11 +15,53 @@ void weirdAlgorithm(int n)
     return weirdAlgorithm(n % 2 == 0 ? n / 2 : n * 3 + 1);
 }
 
+// https://cses.fi/problemset/task/1083
+void missingNumber()
+{
+    std::size_t size;
+    std::cin >> size;
+
+    std::vector<int> input{};
+    for (std::size_t i = 0; i < (size - 1); ++i) // (size - 1) because one element is missing
+    {
+        int num;
+        std::cin >> num;
+        input.push_back(num);
+    }
+
+    std::sort(input.begin(), input.end());
+
+    if (input.front() != 1)
+    {
+        std::cout << '1' << std::endl;
+        return;
+    }
+
+    if (input.back() != size)
+    {
+        std::cout << size << std::endl;
+        return;
+    }
+
+    for (auto it = input.begin() + 1; it != input.end() - 1; ++it)
+    {
+        std::cout << *it << std::endl;
+        auto nextIt = std::next(it);
+        if (nextIt != input.end() && *nextIt != *it + 1)
+        {
+            std::cout << *it + 1 << std::endl;
+            return;
+        }
+    }
+}
+
 int main()
 {
     // int n;
     // std::cin >> n;
     // weirdAlgorithm(n);
+
+    // missingNumber();
 
     return 0;
 }
